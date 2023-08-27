a7d8627d98d0a474d21cede3089d6b74bc6fcef0
Guian Fang <74981769+Enderfga@users.noreply.github.com>
2023-08-27 15:23:57 +0800

    Update platypus.sh

--- accessory/exps/finetune/sg/platypus.sh
+++ accessory/exps/finetune/sg/platypus.sh
@@ -13,11 +13,11 @@ exp_name=finetune/sg/platypus
 echo "exp name: $exp_name"
 mkdir -p output/"$exp_name"
 
-torchrun --master_port=1112 --nproc_per_node=8 main_finetune.py \
---output_dir output/"$exp_name" --epochs 3 --warmup_epochs 0.04 \
---batch_size 4 --accum_iter 4 --num_workers 4 \
---max_words 2048 \
---lr 0.00002 --min_lr 0.0 --clip_grad 2 --weight_decay 0.0 \
+torchrun --master_port=1112 --nproc_per_node=6 main_finetune.py \
+--output_dir output/"$exp_name" --epochs 4 --warmup_epochs 1 \
+--batch_size 4 --accum_iter 2 --num_workers 4 \
+--max_words 512 \
+--lr 0.00003 --min_lr 0.000005 --clip_grad 2 --weight_decay 0.02 \
 --data_parallel "$data_parallel" --model_parallel_size "$model_parallel" --checkpointing \
 --llama_type llama --llama_config "$llama_config" --tokenizer_path "$tokenizer_path" \
 --no_visual \
