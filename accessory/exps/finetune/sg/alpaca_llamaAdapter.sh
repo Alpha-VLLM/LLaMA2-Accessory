70a969a26caca2d34789b5e67819779cff4d7d9b
ChrisLiu6 <ChrisLiudy@outlook.com>
2023-09-04 16:45:46 +0800

    update peft hyperparams

--- accessory/exps/finetune/sg/alpaca_llamaAdapter.sh
+++ accessory/exps/finetune/sg/alpaca_llamaAdapter.sh
@@ -19,7 +19,7 @@ torchrun --master_port=1112 --nproc_per_node=8 main_finetune.py \
 --max_words 512 \
 --lr 0.00005 --min_lr 0.000005 --clip_grad 2 --weight_decay 0.02 \
 --data_parallel "$data_parallel" --model_parallel_size "$model_parallel" --checkpointing \
---llama_type llama_adapter --llama_config "$llama_config" --tokenizer_path "$tokenizer_path" \
+--llama_type llama_adapter --llama_config $llama_config --tokenizer_path "$tokenizer_path" \
 --no_visual \
 --pretrained_path "$pretrained_path" --pretrained_type="$pretrained_type" \
 --data_config $data_config \
